74327288484f4e2bc1986c8c3b37dd2858685f12
Bobacar Boutayeb <104491490+bobacarBoutayeb@users.noreply.github.com>
2023-05-02 15:34:30 +0200

    feat: autologin

--- login.php
+++ login.php
@@ -1,46 +1,20 @@
 <?php
-
-// Define the login page URL
-$login_page_url = 'https://moodle.caseine.org/login/index.php';
-
-// Use cURL to retrieve the HTML of the login page
+// fetch_token.php
+$url = 'https://moodle.caseine.org/login/index.php';
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $login_page_url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$html = curl_exec($ch);
-curl_close($ch);
 
-// Use regular expressions to extract the logintoken value from the HTML
-preg_match('/<input type="hidden" name="logintoken" value="([^"]+)">/', $html, $matches);
-$logintoken = $matches[1];
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 
-// Define the login URL and the data to be sent with the request
-$login_url = 'https://moodle.caseine.org/login/index.php';
-$username = 'testcna';
-$password = '*TestCNA38+';
-$anchor = '';
-$data = array(
-    'username' => $username,
-    'password' => $password,
-    'logintoken' => $logintoken,
-    'anchor' => $anchor
-);
-
-// Use cURL to make the POST request to the login URL with the data
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $login_url);
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $response = curl_exec($ch);
-
-// Check if the login was successful
-if (curl_getinfo($ch, CURLINFO_HTTP_CODE) == 200) {
-    echo 'Login successful!';
-} else {
-    echo 'Login failed!';
-}
-
 curl_close($ch);
 
+preg_match('/name="logintoken" value="(.+?)"/', $response, $matches);
+
+if (!empty($matches[1])) {
+    echo $matches[1];
+} else {
+    echo 'Error: Token not found';
+}
 ?>
